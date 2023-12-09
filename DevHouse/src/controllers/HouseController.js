3f6e9fa0cf94a617a8f09d9e187b2bc8076de468
Yasmin Souza <yasmin075souza@hotmail.com>
2023-12-09 18:26:02 -0300

    update and delete houses

--- DevHouse/src/controllers/HouseController.js
+++ DevHouse/src/controllers/HouseController.js
@@ -1,6 +1,13 @@
 import House from "../models/House"; //access for manipulation
+import User from "../models/User";
 
 class HouseController {
+  async index(req, res) {
+    const { status } = req.query;
+    const houses = await House.find({ status });
+    return res.json(houses);
+  }
+
   async store(req, res) {
     const { filename } = req.file;
     const { description, price, location, status } = req.body;
@@ -17,6 +24,52 @@ class HouseController {
     });
 
     return res.json(house);
+  }
+
+  async update(req, res) {
+    const { filename } = req.file;
+    const { description, price, location, status } = req.body;
+    const { user_id } = req.headers;
+    const { house_id } = req.params;
+
+    const user = await User.findById(user_id);
+    const house = await House.findById(house_id);
+
+    //authorization
+    if (String(user._id) !== String(house.user)) {
+      return res.status(401).json({ error: "Unauthorized user" });
+    }
+
+    await House.updateOne(
+      { _id: house_id },
+      {
+        user: user_id,
+        thumbnail: filename,
+        description,
+        price,
+        location,
+        status,
+      }
+    );
+
+    return res.send();
+  }
+
+  async destroy(req, res) {
+    const { house_id } = req.body;
+    const { user_id } = req.headers;
+
+    const user = await User.findById(user_id);
+    const house = await House.findById(house_id);
+
+    //authorization
+    if (String(user._id) !== String(house.user)) {
+      return res.status(401).json({ error: "Unauthorized user" });
+    }
+
+    await House.findByIdAndDelete({ _id: house_id });
+
+    return res.json({ message: "Sucessfully deleted" });
   }
 }
 
