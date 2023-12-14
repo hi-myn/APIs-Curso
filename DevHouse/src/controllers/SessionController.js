1f29962b164682b9c51c23605493c2ce3c194ab2
Yasmin Souza <yasmin075souza@hotmail.com>
2023-12-14 16:13:52 -0300

    validation email

--- DevHouse/src/controllers/SessionController.js
+++ DevHouse/src/controllers/SessionController.js
@@ -1,16 +1,25 @@
 import User from "../models/User";
+import * as Yup from "yup";
 
 class SessionControler {
   //para fazer login
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+    });
+
     const { email } = req.body;
 
-    //checking if the user exists
-    let user =  await User.findOne({email})
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: "Validation fails" });
+    }
 
-    if(!user) {
-        user = await User.create({ email }); //creating in DB
-    } 
+    //checking if the user exists
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      user = await User.create({ email }); //creating in DB
+    }
 
     return res.json(user);
   }
